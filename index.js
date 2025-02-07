5757fe242356092af18680895eff5f6400747b7d
sparx27 <nicolasgcardani@gmail.com>
2025-02-07 14:38:48 -0300

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 let { notes } = require('./db/notes.js')
 const { generateId } = require('./db/utilities.js')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (_, res) => res.send(`<h1>Working!</h1>`))
